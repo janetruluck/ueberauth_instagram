5f3a24fb15e73649463d6252c506ad89267b0eb5
Jason Truluck <jason@smartlogic.io>
2017-06-12 14:00:56 -0400

    Bump version for bug fixes

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule UeberauthInstagram.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @url "https://github.com/jasontruluck/ueberauth_instagram"
 
   def project do
